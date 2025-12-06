a9603171ca6d51b1886d7d40e67cfe17da9edd86
Ari Cummings <ari@aricummings.com>
2025-12-05 17:49:25 -0800

    Initial commit

---
+++ MileageTrackerMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MileageTrackerMobile.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    
+}
---
+++ MileageTrackerMobile/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MileageTrackerMobile.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
